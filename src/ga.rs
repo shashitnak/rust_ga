71703f11b2137cc96bf6ddb629831423872a8a92
Shashi Kant <shashitnak@gmail.com>
2020-03-13 22:10:20 +0530

    Tested ga for a simple search task

---
+++ src/ga.rs
@@ -0,0 +1,82 @@
+#![allow(unused)]
+
+extern crate rand;
+
+use rand::Rng;
+
+pub fn random() -> f64 {
+    rand::thread_rng().gen()
+}
+
+pub fn radint(num: usize) -> usize {
+    ((num as f64) * random()) as usize
+}
+
+pub fn rex(start: usize, end: usize) -> usize {
+    start + radint(end - start)
+}
+
+pub trait Genome {
+    fn new() -> Self;
+    fn fitness(&self) -> f64;
+    fn cross(&self, other: &Self) -> Self;
+    fn mutate(self) -> Self;
+    fn display(&self);
+}
+
+pub struct Population<T: Genome> {
+    genomes: Vec<T>,
+    prob: Vec<f64>
+}
+
+impl<T: Genome> Population<T> {
+    pub fn new(size: usize) -> Self {
+        Self {
+            genomes: (0..size).map(|_| T::new()).collect(),
+            prob: Vec::new()
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.genomes.len()
+    }
+
+    pub fn live(&mut self) {
+        let mut total_score = 0.;
+        let mut scores = Vec::with_capacity(self.size());
+        let mut fittest: &T = &self.genomes[0];
+        let mut max_score = 0.;
+        
+        for genome in &self.genomes {
+            let score = genome.fitness();
+            scores.push(score);
+            total_score += score;
+
+            if score > max_score {
+                max_score = score;
+                fittest = genome;
+            }
+        }
+
+        scores.iter_mut().for_each(|val| *val = *val / total_score);
+        self.prob = scores;
+        fittest.display();
+    }
+
+    fn pick_one(&self) -> &T {
+        let mut r = random();
+        let mut index = 0;
+        while r > 0. {
+            r -= self.prob[index];
+            index += 1;
+        }
+
+        &self.genomes[index - 1]
+    }
+
+    pub fn next_generation(&mut self) {
+        self.genomes = (0..self.size())
+            .map(|_| self.pick_one().cross(self.pick_one()).mutate())
+            .collect();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+#![allow(unused)]
+
+pub mod ga;
+
+#[cfg(test)]
+mod tests {
+
+    use super::ga::*;
+
+    const TARGET: [char; 20] = ['T', 'o', ' ', 'b', 'e', ',',
+        ' ', 'o', 'r', ' ', 'n', 'o', 't', ' ', 't', 'o', ' ', 'b', 'e', '.'];
+
+    use std::iter::once;
+    
+    fn random_char() -> char {
+        (rex(32, 122) as u8) as char
+    }
+
+    struct Gene {
+        text: [char; 20]
+    }
+
+    impl Genome for Gene {
+        fn new() -> Self {
+            let mut text = [0u8 as char; 20];
+            for i in 0..20 {
+                text[i] = random_char();
+            }
+            Self { text }
+        }
+
+        fn fitness(&self) -> f64 {
+            2f64.powf((0..20).map(|i| ((self.text[i] == TARGET[i]) as usize) as f64).sum())
+        }
+
+        fn cross(&self, other: &Self) -> Self {
+            let mut text = [0u8 as char; 20];
+            for i in 0..20 {
+                text[i] = match radint(2) {
+                    0 => self.text[i],
+                    1 => other.text[i],
+                    _ => random_char()
+                }
+            }
+
+            Self { text }
+        }
+
+        fn mutate(mut self) -> Self {
+            for i in 0..20 {
+                if random() < 0.01 {
+                    self.text[i] = random_char();
+                }
+            }
+            self
+        }
+
+        fn display(&self) {
+            let display_text: String = self.text.iter().cloned().collect();
+            println!("{}", display_text);
+        }
+    }
+
+    #[test]
+    fn test_shakespear() {
+        let mut population: Population<Gene> = Population::new(100);
+        for i in 0..1000 {
+            population.live();
+            population.next_generation();
+        }
+    }
+}
